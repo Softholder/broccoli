e3254ac5d8ad51f3d41374dc563ae713e0556067
Daniel <fcdfl1218@163.com>
2020-03-23 19:29:53 +0800

    init project

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"]
+  // presets: ["@vue/cli-plugin-babel/preset"]
+  presets: [
+    "@vue/app",
+    [
+      "env", //添加 babel-preset-env 配置
+      {
+        modules: false
+      }
+    ]
+  ],
+  plugins: [
+    [
+      "component",
+      {
+        libraryName: "element-ui",
+        styleLibraryName: "theme-chalk"
+      }
+    ]
+  ]
 };
